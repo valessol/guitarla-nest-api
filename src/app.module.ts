31ee6246d5c716230a966adfea9cafdbcac167ed
Valeria Silveira <ssilveira@nosis.com>
2023-03-31 14:34:52 -0300

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,20 @@ import { AppService } from './app.service';
 import { ProductsController } from './products/products.controller';
 import { ProductsService } from './products/products.service';
 import { ProductsModule } from './products/products.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ProductsModule],
-  controllers: [AppController, ProductsController],
-  providers: [AppService, ProductsService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/store'),
+    ProductsModule,
+    UserModule,
+    AuthModule,
+  ],
+  controllers: [ProductsController],
+  providers: [ProductsService],
 })
 export class AppModule {}
+
+//https://blog.logrocket.com/how-build-ecommerce-app-nestjs/
